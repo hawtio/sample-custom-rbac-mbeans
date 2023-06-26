fe22773d48fce8099b47a336cb388e766e9639f6
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2023-06-26 20:30:34 +0900

    quarkus create app

---
+++ src/test/java/io/hawt/examples/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package io.hawt.examples;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
